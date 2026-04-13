fb34c568ef69123e27444e4ddbf16a91e058f8ce
La-paix <dj.lapaix.tech@gmail.com>
2026-04-13 10:57:01 +0200

    Initialize Spring Boot project with Maven wrapper

---
+++ src/main/java/rw/autotrack/autotrack360/Autotrack360Application.java
@@ -0,0 +1,12 @@
+package rw.autotrack.autotrack360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Autotrack360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Autotrack360Application.class, args);
+    }
+}
